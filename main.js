82b6ffd7cdb81311764dc254b32ba46f615389ea
Carlyle Douglas <cdouglascorp@gmail.com>
2020-02-20 09:09:07 -0700

    Add to innerAdjacentHTML to tie in to CSS flip functionality

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ var card1 = new Card('assets/bey1.jpg');
 var card2 = new Card('assets/bey1.jpg');
 var card3 = new Card('assets/bey2.jpg');
 var card4 = new Card('assets/bey2.jpg');
-var card5 = new Card('assets/bey3.jpg');
-var card6 = new Card('assets/bey3.jpg');
+var card5 = new Card('assets/bey3.jpeg');
+var card6 = new Card('assets/bey3.jpeg');
 var card7 = new Card('assets/bey4.jpg');
 var card8 = new Card('assets/bey4.jpg');
 var card9 = new Card('assets/bey5.jpg');
@@ -15,8 +15,16 @@ window.addEventListener('load', displayCards)
 
 function displayCards() {
   deck.cards.forEach(function(card) {
-    sectionRight.insertAdjacentHTML('afterbegin', `<div class="card1 cards">
-      <img src="assets/B img.jpg" alt="">
+    sectionRight.insertAdjacentHTML('afterbegin',
+    `<div class="card1 cards flip-container">
+      <div class="flipper">
+        <div class="front">
+          <img src="${card.defaultImg}">
+        </div>
+        <div class="back">
+          <img src="${card.selectedImg}">
+        </div>
+      </div>
     </div>`)
 
   })
