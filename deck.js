f774d70890662580e4f09c310bf299071ca9dd43
Melissa VanKempen <melissa.vankempen@gmail.com>
2020-02-23 19:48:54 -0700

    trying to remove card

--- deck.js
+++ deck.js
@@ -16,11 +16,26 @@ class Deck {
     this.cards = wholeDeck;
   }
 
-  moveToMatched(event) {
+  moveToMatched() {
+    console.log(this.selectedCards)
     if (this.selectedCards[0].selectedImg === this.selectedCards[1].selectedImg) {
       this.selectedCards[0].matched = true
       this.selectedCards[1].matched = true
+      console.log(this.selectedCards);
       this.matchedCards.push(this.selectedCards)
+      console.log(this.matchedCards.length);
+      this.hideMatched(this.matchedCards)
+    }
+  }
+
+  hideMatched(cards) {
+    console.log(cards.length)
+    for(var i = 0; i < cards.length; i++) {
+      console.log(cards)
+      var id = cards[i].id;
+      console.log(id);
+      document.getElementById(`#${id}`).style.visibility = 'hidden';
+    console.log(document.getElementById(`#${id}`))
     }
   }
 
